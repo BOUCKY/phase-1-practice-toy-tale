532b350fdb3db0c3238fcf472171716a91b10e59
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-05-30 16:06:55 -0400

    working submit new toy + likes

--- src/index.js
+++ src/index.js
@@ -22,12 +22,66 @@ fetch("http://localhost:3000/toys")
     renderToys(data)
   })
 
+
+// find the form and add event listener so the submit button works
+const form = document.getElementById('toyForm')
+form.addEventListener('submit', function(e) {
+  // prevent it from automatically refreshing the page
+  e.preventDefault()
+  // create name variable and get the value of the user input
+  const name = e.target.name.value
+  // create image variable and get the image from user input
+  const image = e.target.image.value
+  //create new element and new "physical" location
+  const newCard = document.createElement("div")
+  // classList allows us to link newCard to css styling
+  newCard.classList.add('card')
+  //creating the location to append each toy to 
+  const toyContainer = document.getElementById("toy-collection")
+  // appending the new element to location
+  toyContainer.append(newCard)
+  //location for newName
+  let newName = document.createElement("h2")
+  //setting newName content to name of toy
+  newName.textContent = name
+  //append newName to newCard
+  newCard.append(newName)
+  //location for newImg
+  let newImg = document.createElement("img")
+  // classList allows us to link newImg to toy-avatar styling
+  newImg.classList.add('toy-avatar')
+  //setting new image content to image of toy
+  newImg.src = image
+  //append newImg to newCard
+  newCard.append(newImg)
+  //location for newLikes
+  let newLikes = document.createElement("p")
+  //setting newName content to name of toy
+  newLikes.textContent = `${0}`
+  //append newLikes to newCard
+  newCard.append(newLikes)
+  // location for likes button
+  let likeBtn = document.createElement("button")
+  // create an event listener so the button actually adds likes
+  // create an anonymous function that calls updateLikes and pass in the toy variable and newLikes so updateLikes knows about them outside this scope
+  likeBtn.addEventListener('click', () => updateNewToyLikes(e, newLikes))
+  //set like button
+  likeBtn.textContent = "Like ❤️"
+  //append like button to card
+  newCard.append(likeBtn)
+  //console.log(newCard)
+  // clear the input section after a task has been created
+  form.reset()
+
+})
+
+
 // new function-get array from DOM
 function renderToys(toysArray) {
-  console.log(toysArray)
+  //console.log(toysArray)
   //forEach-go through each item in array and get info
   toysArray.forEach(function(toy) {
-    console.log(toy)
+    //console.log(toy)
     // create new element and new "physical" location
     const newCard = document.createElement("div")
     // classList allows us to link newCard to css styling
@@ -42,7 +96,7 @@ function renderToys(toysArray) {
     newName.textContent = toy.name
     //append newName to newCard
     newCard.append(newName)
-     //location for newImg
+    //location for newImg
     let newImg = document.createElement("img")
     // classList allows us to link newImg to toy-avatar styling
     newImg.classList.add('toy-avatar')
@@ -50,7 +104,7 @@ function renderToys(toysArray) {
      newImg.src = toy.image
      //append newImg to newCard
     newCard.append(newImg)
-    //location ffor newLikes
+    //location for newLikes
      let newLikes = document.createElement("p")
      //setting newName content to name of toy
      newLikes.textContent = toy.likes
@@ -68,7 +122,7 @@ function renderToys(toysArray) {
     likeBtn.textContent = "Like ❤️"
     //append like button to card
     newCard.append(likeBtn)
-     console.log(newCard)
+    //console.log(newCard)
   })
 }
 
@@ -79,5 +133,18 @@ function updateLikes(toy, p){
   toy.likes++
   //sets the p containg number of likes to the new number of likes
   p.textContent = toy.likes
-  console.log(toy, p)
+  //console.log(toy, p)
+ }
+
+// get the like button to like toys for added toys
+// pass in event object and p to get info
+function updateNewToyLikes(e, p){
+  //set current likes to equal the number value of the paragraph contents
+  let currentLikes = parseInt(p.textContent)
+  //increments likes
+  currentLikes++
+  //sets the p containg number of likes to the new number of likes
+  p.textContent = currentLikes
+  //console.log(currentLikes)
+  //console.log(e, p)
  }
