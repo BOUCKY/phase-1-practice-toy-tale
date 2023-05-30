63dc40594e6333905152d3e6589e733a127fa833
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-05-30 11:24:24 -0500

    info for cards

--- src/index.js
+++ src/index.js
@@ -16,10 +16,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
 fetch("http://localhost:3000/toys")
   .then(function(response) {
-    console.log(response)
     return response.json();
   })
   .then(function(data){
-    console.log(data)
+    renderToys(data)
   })
-  
+
+// new function-get array from DOM
+function renderToys(toysArray) {
+  console.log(toysArray)
+  //forEach-go through each item in array and get info
+  toysArray.forEach(function(toy) {
+    console.log(toy)
+    // create new element and new "physical" location
+    const newCard = document.createElement("div")
+      //moved to line 40
+    //creating the location to append each toy to 
+    const toyContainer = document.getElementById("toy-collection")
+    // appending the new element to location
+    toyContainer.append(newCard)
+    //location for newName
+    let newName = document.createElement("h2")
+    //setting newName content to name of toy
+    newName.textContent = toy.name
+     //location for newName
+    let newImg = document.createElement("img")
+     //setting new image content to image of toy
+     newImg = toy.image
+     let newLikes = document.createElement("p")
+     //setting newName content to name of toy
+     newLikes.textContent = toy.newLikes
+     //append info
+     //let toyInfo = `${newName} ${newImg} ${newLikes}`
+     //newCard.append(toyInfo)
+     console.log(newCard)
+  })
+}
